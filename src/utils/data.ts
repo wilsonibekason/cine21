2b0ecdb673b602a8692402c52c6c6d1a32313fbe
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 16:28:00 +0100

    complete create movie object

---
+++ src/utils/data.ts
@@ -0,0 +1,13 @@
+export const cineMovie = [
+  {
+    img: "",
+    title: "",
+    description: "",
+    showtime: "",
+    rating: "",
+    duration: "",
+    genre: [],
+    cast: [],
+    director: "",
+  },
+];
