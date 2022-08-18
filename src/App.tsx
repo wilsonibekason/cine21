f740337131a4b8cde145a7f22d7c7cbedd189eb6
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 16:01:57 +0100

    complete create app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Feed, Footer,Header, MovieCard, Post,Tickets} from "./components"
+function App() {
+  return (
+  <>
+  <div>
+
+  </div>
+  </>
+  )
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export {default as Header} from "./Header";
+export {default as Footer} from "./Footer";
+export {default as Feed} from "./Feed";
+export {default as MovieCard} from "./MovieCard";
+export {default as Post} from "./Post";
+export {default as Tickets} from "./Tickets";
