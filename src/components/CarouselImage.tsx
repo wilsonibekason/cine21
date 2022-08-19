cd8871b3f2dabf523a4d854ea14580c11098ee45
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-19 14:14:42 +0100

    complete add typescript movie carousel

--- src/components/CarouselImage.tsx
+++ src/components/CarouselImage.tsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { beast, cold } from "../assets";
+import { TodosContextState } from "../state/types";
 // import { useContextMovies } from "../state/oncontextMovies";
 import { carouselAsset } from "../utils/data";
 const CarouselImage = () => {
+  const contextDefaultValues: TodosContextState = {
+    todos: [],
+    addTodo: () => {},
+    currentIndex: 0,
+    count: 0,
+    slideRef: useRef<HTMLDivElement>(null),
+  };
   // const { count, currentIndex, slideRef } = useContextMovies();
+  let slideRef = useRef<HTMLDivElement>(null);
+  const [todos, setTodos] = useState<string[]>(contextDefaultValues.todos);
+  const [currentIndex, setCurrentIndex] = useState<number>(
+    contextDefaultValues.currentIndex
+  );
+  let count = 0;
+  let slideInterval: number | undefined;
+  useEffect(() => {
+    slideRef.current?.addEventListener("animationend", removeAnimation);
+    slideRef?.current?.addEventListener("mouseenter", endSlider);
+    // slideRef?.current?.addEventListener("mouseleave", startSlider());
+    startSlider;
+    // clean up function
+    return () => endSlider();
+  }, []);
+
+  const removeAnimation = () =>
+    slideRef.current?.classList.remove(".fade-anim");
+  const startSlider = setInterval(() => {
+    handleNextSlide();
+  }, 10000);
+  const endSlider = () => clearInterval(slideInterval);
+  const handleOnNext = () => {
+    count = (count + 1) % carouselAsset.length;
+    setCurrentIndex(count);
+    slideRef?.current?.classList.add(".fade-anim");
+  };
+  const handleNextSlide = () => {
+    const productBannerLength = carouselAsset.length;
+    count = (currentIndex + productBannerLength + 1) % productBannerLength;
+    setCurrentIndex(count);
+    slideRef.current?.classList.add(".fade-anim");
+  };
   return (
     <>
       <div
         className="absolute top-0 w-full h-full bg-center bg-cover"
         style={{
-          // backgroundImage: `url('${carouselAsset[currentIndex]}')`,
-          backgroundImage: `url('${beast}')`,
+          backgroundImage: `url('${carouselAsset[currentIndex]}')`,
+          //backgroundImage: `url('${beast}')`,
         }}
-        // ref={slideRef}
+        ref={slideRef}
       >
         <span
           id="blackOverlay"
--- src/state/oncontextMovies.tsx
+++ src/state/oncontextMovies.tsx
@@ -1,70 +1,70 @@
-// import React, {
-//   createContext,
-//   useState,
-//   FC,
-//   useContext,
-//   useEffect,
-//   useRef,
-// } from "react";
-// import { carouselAsset } from "../utils/data";
-// import { TodosContextState, slideProviderProps, RefObject } from "./types";
+import React, {
+  createContext,
+  useState,
+  FC,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
+import { carouselAsset } from "../utils/data";
+import { TodosContextState, slideProviderProps, RefObject } from "./types";
 
-// const contextDefaultValues: TodosContextState = {
-//   todos: [],
-//   addTodo: () => {},
-//   currentIndex: 0,
-//   count: 0,
-//   slideRef: useRef<HTMLDivElement>(null),
-// };
-// export const TodosContext =
-//   createContext<TodosContextState>(contextDefaultValues);
+const contextDefaultValues: TodosContextState = {
+  todos: [],
+  addTodo: () => {},
+  currentIndex: 0,
+  count: 0,
+  slideRef: useRef<HTMLDivElement>(null),
+};
+export const TodosContext =
+  createContext<TodosContextState>(contextDefaultValues);
 
-// const TodosProvider = ({ children }: slideProviderProps) => {
-//   let slideRef = useRef<HTMLDivElement>(null);
-//   const [todos, setTodos] = useState<string[]>(contextDefaultValues.todos);
-//   const [currentIndex, setCurrentIndex] = useState<number>(
-//     contextDefaultValues.currentIndex
-//   );
-//   let count = 0;
-//   let slideInterval: number | undefined;
-//   type GreetFunction = (a: string) => void;
-//   const addTodo = (newTodo: string) => setTodos((todos) => [...todos, newTodo]);
+const TodosProvider = ({ children }: slideProviderProps) => {
+  let slideRef = useRef<HTMLDivElement>(null);
+  const [todos, setTodos] = useState<string[]>(contextDefaultValues.todos);
+  const [currentIndex, setCurrentIndex] = useState<number>(
+    contextDefaultValues.currentIndex
+  );
+  let count = 0;
+  let slideInterval: number | undefined;
+  type GreetFunction = (a: string) => void;
+  const addTodo = (newTodo: string) => setTodos((todos) => [...todos, newTodo]);
 
-//   useEffect(() => {
-//     slideRef.current?.addEventListener("animationend", removeAnimation);
-//     slideRef?.current?.addEventListener("mouseenter", endSlider);
-//     // slideRef?.current?.addEventListener("mouseleave", startSlider());
-//     startSlider;
-//     // clean up function
-//     return () => endSlider();
-//   }, []);
+  useEffect(() => {
+    slideRef.current?.addEventListener("animationend", removeAnimation);
+    slideRef?.current?.addEventListener("mouseenter", endSlider);
+    // slideRef?.current?.addEventListener("mouseleave", startSlider());
+    startSlider;
+    // clean up function
+    return () => endSlider();
+  }, []);
 
-//   const removeAnimation = () =>
-//     slideRef.current?.classList.remove(".fade-anim");
-//   const startSlider = setInterval(() => {
-//     handleOnNext();
-//   }, 2000);
-//   const endSlider = () => clearInterval(slideInterval);
-//   const handleOnNext = () => {
-//     count = (count + 1) % carouselAsset.length;
-//     setCurrentIndex(count);
-//     slideRef?.current?.classList.add(".fade-anim");
-//   };
-//   return (
-//     <TodosContext.Provider
-//       value={{
-//         todos,
-//         addTodo,
-//         count,
-//         currentIndex,
-//         slideRef,
-//       }}
-//     >
-//       {children}
-//     </TodosContext.Provider>
-//   );
-// };
+  const removeAnimation = () =>
+    slideRef.current?.classList.remove(".fade-anim");
+  const startSlider = setInterval(() => {
+    handleOnNext();
+  }, 2000);
+  const endSlider = () => clearInterval(slideInterval);
+  const handleOnNext = () => {
+    count = (count + 1) % carouselAsset.length;
+    setCurrentIndex(count);
+    slideRef?.current?.classList.add(".fade-anim");
+  };
+  return (
+    <TodosContext.Provider
+      value={{
+        todos,
+        addTodo,
+        count,
+        currentIndex,
+        slideRef,
+      }}
+    >
+      {children}
+    </TodosContext.Provider>
+  );
+};
 
-// export const useContextMovies = () => useContext(TodosContext);
+export const useContextMovies = () => useContext(TodosContext);
 
-// export default TodosProvider;
+export default TodosProvider;
