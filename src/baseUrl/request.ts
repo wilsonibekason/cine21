0c94ce1d1fd1126e4006480a35edd9f5039d8570
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-25 13:16:35 +0100

    complete fetch movies from 3rd party API

---
+++ src/baseUrl/request.ts
@@ -0,0 +1,16 @@
+const API_KEY = "4f9765e9022fae1e5dc0f4d9175863c1";
+
+const requests = {
+  fetchTrending: `trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
+function foo<T extends object>(t: T): T {
+  return { ...(t as object) } as T;
+}
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,24 @@
 import { beast, cold, superpet } from "../assets";
 import { images } from "./imageList";
 
+/**
+ * 
+ * adult: false
+backdrop_path: "/ugS5FVfCI3RV0ZwZtBV3HAV75OX.jpg"
+genre_ids: (3) [16, 878, 28]
+id: 610150
+original_language: "ja"
+original_title: "ドラゴンボール超 スーパーヒーロー"
+overview: "The Red Ribbon Army, an evil organization that was once destroyed by Goku in the past, has been reformed by a group of people who have created new and mightier Androids, Gamma 1 and Gamma 2, and seek vengeance against Goku and his family."
+popularity: 7195.285
+poster_path: "/rugyJdeoJm7cSJL1q4jBpTNbxyU.jpg"
+release_date: "2022-06-11"
+title: "Dragon Ball Super: Super Hero"
+video: false
+vote_average: 7.5
+vote_count: 148
+ */
+
 const { img1, img10, img2, img3, img4, img5, img6, img7, img8, img9 } = images;
 export interface ICineType {
   img: string;
